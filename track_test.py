d798a2625818c08c46be58f232efaa12968d399a
gjclark <gjclark@alaska.edu>
2024-06-10 08:52:48 -0800

    feat: black the code

--- track_test.py
+++ track_test.py
@@ -3,10 +3,10 @@ from stepper import Stepper
 import time
 
 # Create the train stepper motor object
-STR_EL = 200*2*32
-el = Stepper(6,7,steps_per_rev = STR_EL)
-el.speed(STR_EL*0.05)
-#el.overwrite_pos_deg(0)
+STR_EL = 200 * 2 * 32
+el = Stepper(6, 7, steps_per_rev=STR_EL)
+el.speed(STR_EL * 0.05)
+# el.overwrite_pos_deg(0)
 el.free_run(1)
-#el.target_deg(180)
-#el.track_target()
+# el.target_deg(180)
+# el.track_target()
