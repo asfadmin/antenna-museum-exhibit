d057b089306f5ba91ce1200be2e1044984deb033
kim <ktfairbanks@alaska.edu>
2025-03-21 09:40:38 -0800

    adds track status, scales properly in other scenes

--- exhibit-frontend/src/track_status/track_course.gd
+++ exhibit-frontend/src/track_status/track_course.gd
@@ -8,7 +8,7 @@ var current_idx = 0
 
 var expected_time: float = 5.0
 var cursor_pos: Vector2 = Vector2.ZERO
-
+@onready var viewport: SubViewport = get_parent() as SubViewport
 #var full_points = []
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,11 +19,17 @@ func _ready() -> void:
 	#self.path_follow_2d = self.path_2d.get_child(0)
 	
 	var _final_path = []
+	var viewport_parent = viewport.get_parent()
+	#var offset = Vector2(viewport.get_parent().size.x / 2, -viewport.get_parent().size.y) / 2.0
 	for point in self.path_2d.curve.get_baked_points():
-		_final_path.append(point + self.path_2d.position)
+		#var offset = Vector2(viewport.size.x, viewport.size.y) / 2.0
+		#print(viewport.size.x)
+		#print(offset)
+		_final_path.append(point) # + offset)
 	
 	self.points = _final_path
 	#self.path_2d.curve.get_closest_point()
+	
 	get_tree().create_timer(1.0).timeout.connect(_active_timer, CONNECT_ONE_SHOT)
 	#var tween = get_tree().create_tween().set_loops(5)
 	#tween.tween_property($Sprite, "modulate", Color.RED, 1)
@@ -47,7 +53,7 @@ func _active_timer():
 	self.current_idx += 1
 	self.queue_redraw()
 	if self.points.size() != self.current_idx:
-		self.sat_position.position = self.points[self.current_idx] - (self.sat_position.size / 2.0)
+		self.sat_position.position = self.points[self.current_idx]
 		get_tree().create_timer(0.1).timeout.connect(_active_timer, CONNECT_ONE_SHOT)
 	else:
 		self.sat_position.visible = false
