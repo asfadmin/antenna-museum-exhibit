3434ff890bef9a76bed29bce5640ff5c8b41214f
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2025-04-30 15:50:36 -0800

    bug: adjusts antenna motion speed slightly

--- exhibit-frontend/src/antenna/antenna.gd
+++ exhibit-frontend/src/antenna/antenna.gd
@@ -10,6 +10,7 @@ var points = []
 var azimuth_data = []
 var elevation_data = []
 var status = 0.0
+var motion_smoothing_factor = 0.01 # TODO: This is based off the fake progress placeholder rate. May need adjusting later to smaller number when working with actual antenna
 func _ready() -> void:
 	#move_to(1,1,1)
 	DataManager.data_loaded.connect(load_data)
@@ -20,10 +21,6 @@ func load_data(data):
 	if data == null:
 		current_idx = 0
 		points = []
-		#completed_course.points = []
-		#sat_position.visible = false
-		#timer.stop()
-		move_to(90.0, 0.0, 0.0, 5.0)
 		return
 	var column_data = data
 
@@ -33,42 +30,26 @@ func load_data(data):
 
 	self.points = []
 	for idx in range(0, len(azimuth_data)):
-		#if autotrack_status[idx] != 0:
 		self.points.append([azimuth_data[idx], elevation_data[idx]])
 
 	var _final_path = []
-	# var offset = Vector2(viewport.get_parent().size.x, viewport.get_parent().size.y) / 2.0
-	#var offset = Vector2(bounding_box.size.x, bounding_box.size.y) * 1.3 # for some reason the scaling changes? not sure why this is different now
-	#for point in azm_elev:
-		#_final_path.append(point + offset  + Vector2.DOWN * 50)
-	#current_idx = 0
-	#self.points = _final_path
-
-	#sat_position.position = self.points[self.current_idx]
-	#sat_position.visible = true
-	
-	#timer.start(0.1)
 
 func _physics_process(delta: float) -> void:
 	if self.points.size() > 0:
 		var idx = mini(roundi(len(self.points) * status), len(self.points) - 1)
 		var azm_ele = self.points[idx]
-		self.move_to(azm_ele[1], azm_ele[0], 0.0, 5.0)
+		self.move_to(azm_ele[1], azm_ele[0], 0.0, delta / motion_smoothing_factor)
 		pass
 	pass
 
-func move_to(elevation, azimuth, train, duration=0.5):
+func move_to(elevation, azimuth, train, duration=1.66):
 	var tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_CIRC)
 	
 	var ele = Quaternion.from_euler(Vector3(deg_to_rad(elevation - 90.0),0.0,0)).normalized()
-	var azi = Quaternion.from_euler(Vector3(0.0, deg_to_rad(azimuth),0)).normalized()
-	#horizontal_rotation_node.quaternion.slerp(horiz, 0.1)
+	var azi = Quaternion.from_euler(Vector3(0.0, deg_to_rad(-azimuth),0)).normalized()
 	tween.tween_method(antenna_elevation.bind(ele), 0.0, 1.0, duration)
 	tween.tween_method(azimuth_rotation.bind(azi), 0.0, 1.0, duration)
-
-	#tween.tween_property(horizontal_rotation_node, "rotation", Vector3(0,deg_to_rad(elevation),0), 0.1)
-	#tween.tween_property(antenna_rotator_node, "rotation", Vector3(deg_to_rad(azimuth),0,0), 0.1)
 
 func antenna_elevation(weight: float, value: Quaternion):
 	antenna_rotator_node.quaternion = antenna_rotator_node.quaternion.slerp(value, weight).normalized()
