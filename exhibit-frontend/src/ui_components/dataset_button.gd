ef04d4756766bc5cc697105d3278db367f61ba81
Tyler Chase <tylercchase@gmail.com>
2025-04-30 14:45:14 -0400

    feat: dataset selector buttons show which one is tracked/selected

--- exhibit-frontend/src/ui_components/dataset_button.gd
+++ exhibit-frontend/src/ui_components/dataset_button.gd
@@ -2,11 +2,46 @@ extends Button
 
 @export var dataset: Dataset
 @export var default = false
+
+var is_tracked_dataset: bool
+var is_selected_dataset: bool
+
+var default_color: Color
+
 func _ready() -> void:
     pressed.connect(_on_pressed)
     if default:
         Events.emit_dataset_selected(dataset)
+    AntennaState.tracked_dataset_changed.connect(_on_tracked_dataset_changed)
+    Events.dataset_selected.connect(_on_selected_dataset_changed)
+    default_color = get_theme_stylebox("normal").bg_color
 
+
+func _on_tracked_dataset_changed(tracked_dataset: Dataset):
+    if tracked_dataset:
+        is_tracked_dataset = tracked_dataset.dataset_id == dataset.dataset_id
+    else:
+        is_tracked_dataset = false
+    _update_background()
+
+func _on_selected_dataset_changed(selected_dataset: Dataset):
+    if selected_dataset:
+        is_selected_dataset = selected_dataset.dataset_id == dataset.dataset_id
+
+    else:
+        is_selected_dataset = false
+    _update_background()
+
+
+func _update_background():
+    var temp = get_theme_stylebox("normal").duplicate()
+    if is_tracked_dataset:
+        temp.bg_color = Color.DARK_GREEN
+    elif is_selected_dataset:
+        temp.bg_color = default_color.lightened(0.1)
+    else:
+        temp.bg_color = default_color
+    add_theme_stylebox_override("normal", temp)
 
 func _on_pressed():
     Events.emit_dataset_selected(dataset)
