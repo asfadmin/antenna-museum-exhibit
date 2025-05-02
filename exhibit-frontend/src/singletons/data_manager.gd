8295fb67d8ecf33fdf5256b672c0be0ef14248ed
SpicyGarlicAlbacoreRoll <ktfairbanks@alaska.edu>
2025-05-02 09:19:43 -0800

    dev: add debug mode

--- exhibit-frontend/src/singletons/data_manager.gd
+++ exhibit-frontend/src/singletons/data_manager.gd
@@ -9,86 +9,88 @@ var loaded_data
 var percent_complete = 0.0
 
 signal percent_complete_changed(percent: float)
+signal debug_mode_toggled(val: bool)
 
 var filenames = {
-    'AQUA': 'aqa.csv',
-    'AURA': 'aur.csv',
-    'IC2': 'ic2.csv',
-    'ICESAT': 'ic2.csv',
-    'OCO2': 'oc2.csv',
-    'SCISAT': 'sci.csv',
-    'SMAP': 'aqa.csv' # not seeing smap in files?
+	'AQUA': 'aqa.csv',
+	'AURA': 'aur.csv',
+	'IC2': 'ic2.csv',
+	'ICESAT': 'ic2.csv',
+	'OCO2': 'oc2.csv',
+	'SCISAT': 'sci.csv',
+	'SMAP': 'aqa.csv' # not seeing smap in files?
 }
 
 var request_timer: Timer
 var current_dataset: Dataset
+var debug_mode: bool = false
 
 signal track_complete
 
 func _ready() -> void:
-    AntennaState.current_action_changed.connect(_on_action_changed)
-    Events.dataset_selected.connect(_update_dataset)
+	AntennaState.current_action_changed.connect(_on_action_changed)
+	Events.dataset_selected.connect(_update_dataset)
 
-    request_timer = Timer.new()
-    request_timer.autostart = false
-    add_child(request_timer)
-    request_timer.timeout.connect(_on_request_timer_timeout)
-    await get_tree().create_timer(0.1).timeout # get the plot to shift into the correct layout since idk what's going on with it
-    load_data('AQUA')
-    clear_data()
-    DataManager.track_complete.connect(_on_track_complete)
-    Events.functional_button_pressed.connect(_on_functional_button_pressed)
+	request_timer = Timer.new()
+	request_timer.autostart = false
+	add_child(request_timer)
+	request_timer.timeout.connect(_on_request_timer_timeout)
+	await get_tree().create_timer(0.1).timeout # get the plot to shift into the correct layout since idk what's going on with it
+	load_data('AQUA')
+	clear_data()
+	DataManager.track_complete.connect(_on_track_complete)
+	Events.functional_button_pressed.connect(_on_functional_button_pressed)
 
 
 func _update_dataset(dataset: Dataset):
-    current_dataset = dataset
+	current_dataset = dataset
 
 
 func _on_functional_button_pressed(type: BackendService.INTERACTION):
-    var backend = BackendService.new()
-    add_child(backend)
-    backend.request_completed.connect(_on_request_completed.bind(backend))
-    if type == BackendService.INTERACTION.STOP:
-        backend.Stop()
-    elif type == BackendService.INTERACTION.SPEED_UP:
-        backend.Speed()
-    elif type == BackendService.INTERACTION.TRACK && current_dataset:
-        AntennaState.set_tracked_dataset(current_dataset)
-        backend.Path(current_dataset.dataset_id)
-    elif type == BackendService.INTERACTION.REHOME:
-        backend.Home()
+	var backend = BackendService.new()
+	add_child(backend)
+	backend.request_completed.connect(_on_request_completed.bind(backend))
+	if type == BackendService.INTERACTION.STOP:
+		backend.Stop()
+	elif type == BackendService.INTERACTION.SPEED_UP:
+		backend.Speed()
+	elif type == BackendService.INTERACTION.TRACK && current_dataset:
+		AntennaState.set_tracked_dataset(current_dataset)
+		backend.Path(current_dataset.dataset_id)
+	elif type == BackendService.INTERACTION.REHOME:
+		backend.Home()
 
 func _on_track_complete():
-    if AntennaState.current_action == BackendService.INTERACTION.TRACK:
-        AntennaState.set_current_action(BackendService.INTERACTION.STOP) # 
-        AntennaState.set_current_action(BackendService.INTERACTION.STOWED) # paths include the rehoming part already
-    elif AntennaState.current_action == BackendService.INTERACTION.REHOME:
-        AntennaState.set_current_action(BackendService.INTERACTION.STOWED)
+	if AntennaState.current_action == BackendService.INTERACTION.TRACK:
+		AntennaState.set_current_action(BackendService.INTERACTION.STOP) # 
+		AntennaState.set_current_action(BackendService.INTERACTION.STOWED) # paths include the rehoming part already
+	elif AntennaState.current_action == BackendService.INTERACTION.REHOME:
+		AntennaState.set_current_action(BackendService.INTERACTION.STOWED)
 
 func _on_action_changed(action: BackendService.INTERACTION):
-    if action == BackendService.INTERACTION.TRACK:
-        load_data(current_dataset.dataset_id)
-        start_tracking()
-    elif action == BackendService.INTERACTION.STOP:
-        clear_data()
-        AntennaState.set_tracked_dataset(null)
-        stop_tracking() # stopping should pause this, rehoming should reset and start tracking again, for now tho this works
-    elif action == BackendService.INTERACTION.REHOME:
-        clear_data()
-        AntennaState.set_tracked_dataset(null)
-        stop_tracking()
-        start_tracking()
+	if action == BackendService.INTERACTION.TRACK:
+		load_data(current_dataset.dataset_id)
+		start_tracking()
+	elif action == BackendService.INTERACTION.STOP:
+		clear_data()
+		AntennaState.set_tracked_dataset(null)
+		stop_tracking() # stopping should pause this, rehoming should reset and start tracking again, for now tho this works
+	elif action == BackendService.INTERACTION.REHOME:
+		clear_data()
+		AntennaState.set_tracked_dataset(null)
+		stop_tracking()
+		start_tracking()
 
 func load_data(dataset_id):
-    load_column_data(filenames[dataset_id])
-    data_loaded.emit(loaded_data)
+	load_column_data(filenames[dataset_id])
+	data_loaded.emit(loaded_data)
 
 func get_data():
-    return loaded_data
+	return loaded_data
 
 func clear_data():
-    loaded_data = null
-    data_loaded.emit(loaded_data)
+	loaded_data = null
+	data_loaded.emit(loaded_data)
 
 var ACTUAL_AZIMUTH_COLUMN = "Actual Az."
 var ACTUAL_ELEVATION_COLUMN = "Actual El."
@@ -100,35 +102,35 @@ const LHC_COLUMN_NAME := "Antenna Control Unit S-LHC Strength (dB)"
 const RHC_COLUMN_NAME := "Antenna Control Unit S-RHC Strength (dB)"
 
 func get_csv(filename: String) -> Dictionary:
-    var file: FileAccess = FileAccess.open("res://assets/data/%s" % filename, FileAccess.READ)
+	var file: FileAccess = FileAccess.open("res://assets/data/%s" % filename, FileAccess.READ)
 
 	# Get headers to index into content using column names
-    var headers := file.get_csv_line()
-    var header_dict := {}
+	var headers := file.get_csv_line()
+	var header_dict := {}
 
-    for i in headers.size():
-        header_dict[headers[i]] = i
+	for i in headers.size():
+		header_dict[headers[i]] = i
 
-    var content := []
-    # Get the data itself (csv rows)
-    while file.get_position() < file.get_length():
-        var csv_line := file.get_csv_line()
+	var content := []
+	# Get the data itself (csv rows)
+	while file.get_position() < file.get_length():
+		var csv_line := file.get_csv_line()
 
-        if csv_line != null and csv_line.size() > 0:
-            content.append(csv_line)
-        else:
-            print("Empty line")
+		if csv_line != null and csv_line.size() > 0:
+			content.append(csv_line)
+		else:
+			print("Empty line")
 
-    file.close()
-    return {
+	file.close()
+	return {
 		"headers": header_dict,
 		"content": content,
 	}
 
 func load_column_data(filename) -> Dictionary:
-    var csv := get_csv(filename)
+	var csv := get_csv(filename)
 
-    var column_data := {
+	var column_data := {
 		"actual_azimuth": get_csv_column_data(csv["headers"][ACTUAL_AZIMUTH_COLUMN], csv["content"]),
 		"actual_elevation": get_csv_column_data(csv["headers"][ACTUAL_ELEVATION_COLUMN], csv["content"]),
 		"commanded_azimuth": get_csv_column_data(csv["headers"][COMMANDED_AZIMUTH_COLUMN], csv["content"]),
@@ -140,57 +142,67 @@ func load_column_data(filename) -> Dictionary:
 	}
 
 	# The csv data is heavy
-    csv = {}
-    loaded_data = column_data
-    return column_data
+	csv = {}
+	loaded_data = column_data
+	return column_data
 
 
 ## Takes `column_index` and returns the column at that index in the 2d Array `content`.
 func get_csv_column_data(column_index: int, content: Array) -> Array[float]:
-    var column_data: Array[float] = []
-    var content_size: int = content.size()
+	var column_data: Array[float] = []
+	var content_size: int = content.size()
 
-    column_data.resize(content_size)
+	column_data.resize(content_size)
 
-    for i in range(0, content_size):
-        column_data[i] = float(content[i][column_index])
+	for i in range(0, content_size):
+		column_data[i] = float(content[i][column_index])
 
-    return column_data
+	return column_data
 
 
 var fake_progress = 0.0
 
 # this can show even if it fails, will need some error checking possibly
 func _on_request_completed(result: int, response_code: int, headers: PackedStringArray, body: PackedByteArray, node: Node, is_status=false):
-    var converted = body.get_string_from_utf16() # this should give us a JSON response?
-
-    if is_status:
-        var test = {
-            'progress': fake_progress # this will probably be some math later
-        }
-
-        fake_progress += 0.01
-        if fake_progress >= 1.0:
-            stop_tracking()
-            movement_complete()
-        percent_complete = fake_progress
-        percent_complete_changed.emit(percent_complete)
-    node.queue_free()
+	var converted = body.get_string_from_utf8() # this should give us a JSON response?
+	var json_body = {}
+	if len(converted) > 0:
+		json_body = JSON.parse_string(converted)
+	if is_status:
+		if self.debug_mode:
+			fake_progress += 0.01
+			if fake_progress >= 1.0:
+				stop_tracking()
+				movement_complete()
+			percent_complete = fake_progress
+		else:
+			var percentage = json_body.get('progress', 0.0)
+			if percentage >= 1.0:
+				stop_tracking()
+				movement_complete()
+			percent_complete = percentage
+		
+		percent_complete_changed.emit(percent_complete)
+			
+	node.queue_free()
 
 func _on_request_timer_timeout():
-    var backend = BackendService.new()
-    add_child(backend)
-    backend.request_completed.connect(_on_request_completed.bind(backend,true))
-    backend.Status()
+	var backend = BackendService.new()
+	add_child(backend)
+	backend.request_completed.connect(_on_request_completed.bind(backend,true))
+	backend.Status()
 
 func start_tracking():
-    fake_progress = 0.0
-    request_timer.start(0.5)
+	fake_progress = 0.0
+	request_timer.start(0.5)
 
 func stop_tracking():
-    request_timer.stop()
-    percent_complete_changed.emit(0.0)
+	request_timer.stop()
+	percent_complete_changed.emit(0.0)
 
 func movement_complete():
-    track_complete.emit()
+	track_complete.emit()
 
+func toggle_debug_mode():
+	self.debug_mode = !self.debug_mode
+	self.debug_mode_toggled.emit(self.debug_mode)
